79a301cbe4112503569227dfc7f4a9a82b4f0b62
hashini0123 <nanayakkarahashini5@gmail.com>
2026-08-17 23:51:46 +0530

    Update CORS settings for frontend ports

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -147,5 +147,9 @@ REST_FRAMEWORK = { 'DEFAULT_AUTHENTICATION_CLASSES': ( 'rest_framework_simplejwt
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    "http://localhost:5174",
+    "http://127.0.0.1:5174",
+    "http://localhost:5175",
+    "http://127.0.0.1:5175",
 ]
 
